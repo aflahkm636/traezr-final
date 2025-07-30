d1c17387a228e33d174f31605ca65fe87bb6778a
aflahkm636 <aflahkm007@gmail.com>
2025-07-30 11:36:05 +0530

    deployed

--- database/index.js
+++ database/index.js
@@ -1,11 +1,13 @@
-const jsonServer = require("json-server");
+import jsonServer from "json-server";
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults({ static: false }); // disables /public serving
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(10000, () => {
-  console.log("JSON Server is running on port 10000");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
